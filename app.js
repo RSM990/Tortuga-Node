ebd7295c937a0f854e9ec0abab47c8b9e0302a6d
Reagan Michaelson <ReaganSMichaelson@gmail.com>
2025-04-04 13:59:01 -0400

    add path for health check

--- app.js
+++ app.js
@@ -36,6 +36,10 @@ app.use('/movies', movieRoutes);
 // app.use('/order', orderRoutes);
 // app.use('/cart', cartRoutes);
 
+app.get('/', (req, res) => {
+  res.send('OK');
+});
+
 app.use((req, res, next) => {
   res.status(404).send('<h1>Page not found</h1>');
 });
