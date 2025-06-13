0546a4071743befbc464940e556071d91f5d9896
Reagan Michaelson <reagan@mac.myfiosgateway.com>
2025-06-13 14:26:03 -0400

    Test Depoyment Update

--- controllers/test.js
+++ controllers/test.js
@@ -1,6 +1,6 @@
 exports.getTest = (req, res, next) => {
   res.status(200).json({
-    message: 'Test succeeded',
+    message: 'Test succeeded!!!',
     timestamp: new Date().toISOString(),
     environment: process.env.NODE_ENV || 'development',
   });
