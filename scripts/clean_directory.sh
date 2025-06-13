6036b3bd7d52103f9774c98a8ea897657817aee9
Reagan Michaelson <reagan@mac.myfiosgateway.com>
2025-06-13 13:57:24 -0400

    Update yml and clean directory script for code deploy errors

--- scripts/clean_directory.sh
+++ scripts/clean_directory.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
 echo "Cleaning up /home/ec2-user/Tortuga-Node"
-rm -rf /home/ec2-user/Tortuga-Node/*
+
+# Remove entire directory (including .git folder)
+rm -rf /home/ec2-user/Tortuga-Node
+
+# Recreate the directory so CodeDeploy has somewhere to deploy to
+mkdir -p /home/ec2-user/Tortuga-Node
+
+echo "Cleaned up successfully."
