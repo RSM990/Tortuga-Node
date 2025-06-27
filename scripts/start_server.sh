bbb4065e95c62dbb4a1a969d40823a57306d2d23
Reagan Michaelson <reagan@mac.myfiosgateway.com>
2025-06-26 21:23:30 -0400

    Update start server script

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -3,5 +3,8 @@
 echo "Starting Node server"
 cd /home/ec2-user/Tortuga-Node
 
+# explicitly set production mode
+# export NODE_ENV=production
+
 # Example: start your server in background with nohup
 nohup npm start > app.log 2>&1 &
