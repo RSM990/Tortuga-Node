671c166b4810eb636ed551270c887a62efe18dd0
Reagan Michaelson <reagan@mac.myfiosgateway.com>
2025-06-30 17:04:15 -0400

    another cleanup of server scripts

--- scripts/install_dependencies.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo "Installing Node dependencies"
-cd /home/ec2-user/Tortuga-Node
-npm ci
--- scripts/stop_server.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "Stopping Node server (if running)"
-# Kill any Node processes running on your app port
-# Replace 3000 with your app port if necessary
-fuser -k 3000/tcp || true
