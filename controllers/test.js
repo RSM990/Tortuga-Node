91c40fcbbeb8c07608061df53f717f5a3bde0ddd
Reagan Michaelson <reagan@mac.myfiosgateway.com>
2025-06-30 15:22:29 -0400

    Test Deployment for new EC2 instance

--- controllers/test.js
+++ controllers/test.js
@@ -1,6 +1,6 @@
 exports.getTest = (req, res, next) => {
   res.status(200).json({
-    message: 'Test succeeded!!!',
+    message: 'Test route succeeded!!!',
     timestamp: new Date().toISOString(),
     environment: process.env.NODE_ENV || 'development',
   });
