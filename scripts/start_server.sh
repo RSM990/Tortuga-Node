c143a270b8b15b1e7f512ccd72882d714c653224
Reagan Michaelson <reagan@mac.myfiosgateway.com>
2025-06-30 14:55:59 -0400

    another start server test

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -14,7 +14,7 @@ npm install
 pm2 stop tortuga-app || true
 
 # Start the app using pm2 with a consistent name
-pm2 start ecosystem.config.js --env production
+pm2 start index.js --name tortuga-app
 
 # Save the current PM2 process list
 pm2 save
